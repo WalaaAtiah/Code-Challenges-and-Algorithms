70c8279fb0292696b69c9b19c0bdfbe5cae25728
WalaaAtiah <walaaatiah97@gmail.com>
2022-10-30 00:48:57 +0000

    queue

--- python/code_challenges/stack_queue/challenge01/challenge01.py
+++ python/code_challenges/stack_queue/challenge01/challenge01.py
@@ -3,11 +3,11 @@ from unittest import result
 from stack import Stack
 
 #creat queue class
-class Queue:
+class MyQueue :
     '''
     this class Implement a first in first out (FIFO) queue using only two stacks. 
     have 5 method  ( __str__(),push(), peek() , pop(), and empty()).
-    and have 2 attribute (stack1 ,stack_rev)
+    and have 2 attribute (stack1 ,stack_rev self.size, self.result)
     '''
     def __init__(self):
         """
@@ -20,23 +20,48 @@ class Queue:
 
     def __str__(self):
         return "Implement a first in first o (FIFO) queue"
+
     def push (self,value):
+        """
+         to Pushe element value to the back of the queue.
+        """
+        if len(self.result)!=0:
+            for i in self.result:
+                self.stack1.push(i)   
         self.result.append(value)
-        self.stack1.push(value)
+        self.stack1.push(value)  
         self.size +=1
-        return self.result
 
     def pop(self):
+        """
+        Removes the element from the front of the queue and returns it.
+        """
+        if self.size ==0:
+            return "This queue is empty"
         while not self.stack1.is_empty():
             self.stack_rev.push(self.stack1.pop())
+        
         self.size -=1
+        self.result.pop(0)
         return self.stack_rev.pop()
+
     def peek(self):
+        """
+        Returns the element at the front of the queue.
+        """
+        if self.size ==0:
+            return "This queue is empty"
         while not self.stack1.is_empty():
             self.stack_rev.push(self.stack1.pop())
         return self.stack_rev.peek()
+
     def empty(self):
+        """
+         Returns true if the queue is empty, false otherwise.
+        """
         return self.size == 0
+    def print_as_list(self):
+        return self.result
 
 
 if __name__ =="__main__":
@@ -44,12 +69,20 @@ if __name__ =="__main__":
     Welcome to queue data structure
     """)
     # creat queue 
-    queue1=Queue()
+    queue1=MyQueue ()
     # add a new element to the queue py use push methode [1,2]
-    print("queue is: ", queue1.push(1))
-    print("queue is: ",queue1.push(2))
+    queue1.push(1)
+    print("queue is: ", queue1.print_as_list())
+    queue1.push(2)
+    print("queue is: ",queue1.print_as_list())
+    queue1.push(3)
+    print("queue is: ",queue1.print_as_list())
 
     print("queue1.peek() : >> ",queue1.peek())
 
     print("queue1.pop() :>> ",queue1.pop())
+    queue1.push(4)
+    print("queue is: ",queue1.print_as_list())
+    print("queue1.pop() :>> ",queue1.pop())
+   
     print("queue1.empty() :>> ",queue1.empty())
--- python/code_challenges/stack_queue/challenge01/test_challenge01.py
+++ python/code_challenges/stack_queue/challenge01/test_challenge01.py
@@ -1,20 +1,72 @@
 # Write your test here
 from inspect import stack
+import pytest
 from stack import Stack
+from challenge01 import MyQueue 
 
 #test for the stack 
-
-def test_stack():
+@pytest.fixture
+def stack():
     stack1  = Stack()
     stack1.push("H")
     stack1.push("E")
     stack1.push("L")
     stack1.push("L")
     stack1.push("O")
-    assert stack1.pop()=="O"
-    assert stack1.get_size()==4
-    # print(stack1.pop())
-    # print(stack1.pop())
-    # print(stack1.pop())
-    # print(stack1.peek())
-    # print(stack1.get_size())
+    return stack1
+
+def test_stack_one(stack):
+    assert stack.pop()=="O"
+def test_stack_two(stack):
+    assert stack.get_size()==5
+
+
+
+@pytest.fixture
+def queue1():
+    return  MyQueue ()
+
+
+
+def test_queue_one(queue1):
+    assert queue1.__str__()=="Implement a first in first o (FIFO) queue"
+
+def test_queue_two(queue1):
+    queue1.push(1)
+    queue1.push(2)
+    queue1.push(3)
+    queue1.push(4)
+    assert queue1.print_as_list()==[1,2,3,4]
+
+def test_queue_three(queue1):
+    assert queue1.empty()==True
+
+def test_queue_four(queue1):
+    assert queue1.pop()=="This queue is empty"
+
+def test_queue_five(queue1):
+    assert queue1.peek()=="This queue is empty"
+
+def test_queue_pop(queue1):
+    queue1.push(1)
+    queue1.push(2)
+    queue1.push(3)
+    queue1.push(4)
+    assert queue1.pop()==1
+
+def test_queue_peek(queue1):
+    queue1.push(1)
+    queue1.push(2)
+    queue1.push(3)
+    queue1.push(4)
+    assert queue1.peek()==1
+
+def test_have_Push_After_Pop(queue1):
+    queue1.push(1)
+    queue1.push(2)
+    queue1.push(3)
+    queue1.push(4)
+    queue1.pop() # delete the first item 1
+    queue1.pop() # delet the secand item 2
+    queue1.push(66)
+    assert queue1.print_as_list()==[3, 4, 66]
